f05b46f403b6d35dbe058a97b11b67e443b1c59d
nossatvangola2026-sudo <angoladenuncias@gmail.com>
2026-06-22 22:15:49 +0100

    remove: retirar secção Desenvolvido por e NIF da Ficha Técnica

--- src/pages/TechnicalSheetPage.tsx
+++ src/pages/TechnicalSheetPage.tsx
@@ -29,14 +29,7 @@ const TechnicalSheetPage = () => {
                                 <p className="font-bold uppercase text-xs text-primary mb-0">Redacção</p>
                                 <p className="text-base">Equipa Sem Filtros</p>
                             </div>
-                            <div>
-                                <p className="font-bold uppercase text-xs text-primary mb-0">Desenvolvido por</p>
-                                <p className="text-base italic">BYTEKWANZA - PRESTAÇÃO DE SERVIÇOS, LDA.</p>
-                            </div>
-                            <div>
-                                <p className="font-bold uppercase text-xs text-primary mb-0">NIF</p>
-                                <p className="text-base">5003198294</p>
-                            </div>
+
                         </div>
                     </div>
 
