d52a68296d0c5aa71408fa02a3eb7083fa64e15d
nossatvangola2026-sudo <angoladenuncias@gmail.com>
2026-07-04 07:30:41 +0100

    chore: inspect GEMINI_API_KEY length

--- api/debug.js
+++ api/debug.js
@@ -5,12 +5,16 @@ export const config = {
 export default async function handler(req) {
     const keys = Object.keys(process.env || {});
     const hasGemini = !!(process.env.GEMINI_API_KEY || process.env.GOOGLE_AI_STUDIO_API_KEY);
+    const geminiLength = (process.env.GEMINI_API_KEY || "").length;
+    const googleLength = (process.env.GOOGLE_AI_STUDIO_API_KEY || "").length;
     const nodeEnv = process.env.NODE_ENV;
 
     // We print keys that contain sensitive words without showing values
     return new Response(JSON.stringify({
         keys,
         hasGemini,
+        geminiLength,
+        googleLength,
         nodeEnv
     }), {
         headers: { 'Content-Type': 'application/json' },
